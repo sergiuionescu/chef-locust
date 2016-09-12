3b0512ad74bbbe6a16480686323b9de4eb0299fb
Al Lefebvre <alain.lefebvre@gmail.com>
2016-09-12 10:02:49 -0400

    Committing current changes

--- recipes/_test_master.rb
+++ recipes/_test_master.rb
@@ -4,7 +4,7 @@ locustio_node 'tester' do
   report_to_datadog true
   test_file '_test_virtual_user.py'
   script_parameters({'uri_list' => {'uri' => '/', 'weight' => '20', 'identifier' => 'home'}})
-  autostart true
+  autostart false
   autostart_total_virtual_users 100
   autostart_hatch_rate 5
 end
